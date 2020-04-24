79b5135bc1a7a629db29bf7a4d297a997d3b098e
Gleb Stashinsky <tragy@list.ru>
2020-04-24 22:46:55 +0300

    Deleted unused dependency

--- src/test/java/com/gleb/st/debt_count/calculator/SomeDebtCalculationTests.java
+++ src/test/java/com/gleb/st/debt_count/calculator/SomeDebtCalculationTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
