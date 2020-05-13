f120fc882369548bd07055ebd65143843bedd939
Gleb Stashinsky <tragy@list.ru>
2020-05-14 00:08:04 +0300

    Rewrite test using Java 14

--- src/test/java/com/gleb/st/debt_count/calculator/SomeDebtCalculationTests.java
+++ src/test/java/com/gleb/st/debt_count/calculator/SomeDebtCalculationTests.java
@@ -1,19 +1,17 @@
 package com.gleb.st.debt_count.calculator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.gleb.st.debt_count.controller.rest.CalculationController;
 import com.gleb.st.debt_count.entity.calculation.Calculation;
 import com.gleb.st.debt_count.entity.calculation.CalculationInputData;
-import com.gleb.st.debt_count.entity.debtor.Bill;
-import com.gleb.st.debt_count.entity.debtor.Payment;
-import com.gleb.st.debt_count.service.calculator.DebtCalculationService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * долг 22 138,00
@@ -33,38 +31,52 @@ import java.util.List;
 @SpringBootTest
 public class SomeDebtCalculationTests {
 
+    private final String DATA = """
+                        {
+                         "bills":
+                           [{
+                             "amount":22138.0,
+                             "paymentDate":"2019-03-02",
+                             "payments":
+                               [{
+                                 "amount":"4305.19",
+                                 "date":"2019-11-26"
+                               },
+                               {
+                                 "amount":"1818.18",
+                                 "date":"2019-12-03"
+                               },
+                               {
+                                 "amount":"1818.18",
+                                 "date":"2019-12-19"
+                               },
+                               {
+                                 "amount":"1818.18",
+                                 "date":"2019-12-26"
+                               },
+                               {
+                                 "amount":"12378.26",
+                                 "date":"2020-01-03"
+                               }]
+                           }],
+                         "calculationDate":"2020-05-13"
+                        }
+                        """;
+
     @Autowired
-    private DebtCalculationService calculationService;
+    private CalculationController controller;
 
-    private Calculation processCalculation() {
-        CalculationInputData inputData = new CalculationInputData();
+    private Calculation processCalculation() throws JsonProcessingException {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        List<Payment> payments = new ArrayList<>(5);
-        payments.add(new Payment(LocalDate.parse("26.11.2019", formatter), 4_305.19));
-        payments.add(new Payment(LocalDate.parse("03.12.2019", formatter), 1_818.18));
-        payments.add(new Payment(LocalDate.parse("19.12.2019", formatter), 1_818.18));
-        payments.add(new Payment(LocalDate.parse("26.12.2019", formatter), 1_818.18));
-        payments.add(new Payment(LocalDate.parse("03.01.2020", formatter), 12_378.26));
-
-        Bill bill = new Bill();
-        bill.setAmount(22_138.0);
-        bill.setPayments(payments);
-        bill.setPaymentDate(LocalDate.parse("02.03.2019", formatter));
-
-        List<Bill> bills = new ArrayList<>(1);
-        bills.add(bill);
-
-        inputData.setBills(bills);
-
-        inputData.setCalculationDate(LocalDate.now());
-
-        return calculationService.processCalculation(inputData);
+        return controller.makeCalculation(mapper.readValue(DATA, CalculationInputData.class));
     }
 
     @Test
-    public void percentCalculationTest() {
+    public void percentCalculationTest() throws JsonProcessingException {
         Calculation calculation = processCalculation();
         Assert.assertEquals(1_616.73, calculation.getPercent(), 0.01);
     }
