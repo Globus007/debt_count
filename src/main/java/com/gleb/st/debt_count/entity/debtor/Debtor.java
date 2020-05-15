5f0e4cf5b266e5c941310a1983c6d7578d815c75
Gleb Stashinsky <tragy@list.ru>
2020-05-15 13:41:27 +0300

    Rewrite class Creditor and Debtor to record

--- src/main/java/com/gleb/st/debt_count/entity/debtor/Debtor.java
+++ src/main/java/com/gleb/st/debt_count/entity/debtor/Debtor.java
@@ -1,48 +1,7 @@
 package com.gleb.st.debt_count.entity.debtor;
 
-public class Debtor {
-
-    private int id;
-    private String name;
-    private String props;
-
-    public Debtor() {}
-
-    public Debtor(String name, String address) {
-        this.name = name;
-        this.props = address;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProps() {
-        return props;
-    }
-
-    public void setProps(String props) {
-        this.props = props;
-    }
-
-    @Override
-    public String toString() {
-        return "Debtor{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", props='" + props + '\'' +
-                '}';
-    }
-}
+public record Debtor (
+        int id,
+        String name,
+        String props
+) {}
